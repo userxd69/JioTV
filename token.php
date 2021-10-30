1fe23fa029fd0973d7f496c8931c2443662582bf
userxd69 <89330562+userxd69@users.noreply.github.com>
2021-10-30 12:45:34 +0530

    Update token.php

--- token.php
+++ token.php
@@ -2,14 +2,12 @@
 
 # © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
 
-# Here I Put Token which Available Publicly I Recommended Use Your Own Token Here 
-# For Suppport @Avishkarpatil at Telegram or proavipatil@gmail.com
 
 
 
 $jctBase = "cutibeau2ic";
 
-$ssoToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiI2ZTlhY2RiOC1kMjVhLTQ0YTgtYTMwNi1hNTgwYmUwMTllYmEiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjEwIiwiZGV2aWNlSWQiOm51bGwsImp0aSI6Ijc1MWUwNTY5LTA4ZGYtNDc1Ni05YmZkLTczY2YxOTY1MGM1NSIsImlhdCI6MTYyOTg5NjI0N30.3t50ZraZNu2T6GV0MP73yi2YXKTzrPAsYV4gtpy4UPU"; #Change This with your SSOTOKEN 
+$ssoToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiI5YjVlY2Y2MS05MDY0LTQ4NWYtOTk0MC05ZDI4YmQ4MTVhODIiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiI4ODAyNjZhY2I1NDcyYmJjYWYwZTY0ZWE2YmE0NWE2ZDM1ZTAxNzVkZWQ2NTk4OTIwNGM0NDRhNzlhN2U3MWM4MDA0YWVkNTJlMDgyYWYzN2JkYjk1NGJiNTYxYjM3YmMxOWViOWMzNWMwZmI2MWFjOWVlZjE2ZjM3OTMwOWExNiIsImp0aSI6IjI4MjUyZjMwLTJlMzUtNDczNS1hNDc5LWJlN2ExYTI0NmZmMiIsImlhdCI6MTYzNDk5ODE5N30.q5Cm3b6FJ5MuWNA35OmDDnXU4cgezmnZSzhQcNqorwU"; #Change This with your SSOTOKEN 
 function tokformat($str)
 {
 $str= base64_encode(md5($str,true));
@@ -34,7 +32,7 @@ $jct = generateJct($st, $pxe);
 return "?jct=" . $jct . "&pxe=" . $pxe . "&st=" . $st;
 }
 
-# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
 
 
 echo generateToken();
